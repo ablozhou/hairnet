2400b719655e038830f20caa731c94a5c0d98a53
huzhiming123 <huzhiming123@0839bf12-9853-0410-b893-9948c926453d>
2008-11-08 14:18:15 +0000

    9．	信息删除，删除美发厅，美发厅的图片未删除，推荐信息未删除等

--- trunk/Web/Admin/HairShopAdmin.aspx.cs
+++ trunk/Web/Admin/HairShopAdmin.aspx.cs
@@ -360,6 +360,26 @@ namespace Web.Admin
                             }
                         }
 
+                        using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["MSSqlServer"].ConnectionString))
+                        {
+                            string commString = "delete from HairShopRecommand where HairShopRawID=" + hairShopID.ToString();
+                            using (SqlCommand comm = new SqlCommand())
+                            {
+                                comm.CommandText = commString;
+                                comm.Connection = conn;
+                                conn.Open();
+
+                                try
+                                {
+                                    comm.ExecuteNonQuery();
+                                }
+                                catch
+                                {
+
+                                }
+                            }
+                        }
+
                         StringHelper.AlertInfo("删除成功", this.Page);
                         this.Response.Redirect("HairShopAdmin.aspx");
                     }
